a507e399ac387e3c97517dc4d7fc4a637386c799
doriav20 <doriav20@gmail.com>
2023-10-22 22:45:43 +0300

    Implement Client

---
+++ client/src/BoardDisplay.js
@@ -0,0 +1,54 @@
+import './BoardDisplay.css';
+import useTicTacToe, { GameStatus } from './useTicTacToe';
+
+function BoardDisplay() {
+    const { board, handleClick, numberOfColumns, startGame, gameStatus, inGameStatus } = useTicTacToe();
+
+    if (gameStatus === GameStatus.WAITING_FOR_OPPONENT) {
+        return (
+            <div>
+                <p>Waiting for opponent...</p>
+            </div>
+        );
+    }
+
+    const readyToStart = gameStatus === GameStatus.READY_TO_START;
+
+    const BoardComponent = () => {
+        return (
+            board &&
+            board.map((row, i) => (
+                <div className="row" key={i}>
+                    {row.map((col, j) => {
+                        const index = i * numberOfColumns + j;
+                        return (
+                            <button className="cell" key={index} onClick={() => handleClick(i, j)}>
+                                {board[i][j]}
+                            </button>
+                        );
+                    })}
+                </div>
+            ))
+        );
+    };
+
+    return (
+        <>
+            <div className="game-status">
+                <p>{inGameStatus}</p>
+            </div>
+            <div>
+                <BoardComponent />
+            </div>
+            {readyToStart && (
+                <div>
+                    <button className="start-game-button" onClick={startGame}>
+                        Start Game
+                    </button>
+                </div>
+            )}
+        </>
+    );
+}
+
+export default BoardDisplay;
---
+++ client/src/useTicTacToe.js
@@ -0,0 +1,119 @@
+import { useEffect, useRef, useState } from 'react';
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:3001');
+
+export const GameStatus = Object.freeze({
+    READY_TO_START: 'READY_TO_START',
+    WAITING_FOR_OPPONENT: 'WAITING_FOR_OPPONENT',
+    IN_PROGRESS: 'IN_PROGRESS',
+});
+
+export const InGameStatus = Object.freeze({
+    YOUR_TURN: 'Your turn',
+    // eslint-disable-next-line quotes
+    OPPONENT_TURN: "Opponent's turn",
+    YOU_WON: 'You Won!',
+    YOU_LOST: 'You Lost :(',
+    DRAW: 'Draw',
+    OPPONENT_DISCONNECTED: 'Opponent Disconnected',
+});
+
+function useTicTacToe() {
+    const [board, setBoard] = useState(null);
+    const [numberOfRows, setNumberOfRows] = useState(0);
+    const [numberOfColumns, setNumberOfColumns] = useState(0);
+    const [player, setPlayer] = useState(null);
+    const [gameStatus, setGameStatus] = useState(GameStatus.READY_TO_START);
+    const [inGameStatus, setInGameStatus] = useState(null);
+    const inGameStatusRef = useRef(inGameStatus);
+    const playerRef = useRef(player);
+
+    function sendClickToServer(i, j) {
+        socket.emit('cell_click', i, j);
+    }
+
+    function handleClick(i, j) {
+        if (board[i][j] !== null || inGameStatus !== InGameStatus.YOUR_TURN) {
+            return;
+        }
+
+        setBoard((prevBoard) => {
+            const newBoard = [...prevBoard];
+            newBoard[i][j] = player;
+            return newBoard;
+        });
+        sendClickToServer(i, j);
+    }
+
+    function startGame() {
+        socket.emit('start_game');
+        setGameStatus(GameStatus.WAITING_FOR_OPPONENT);
+    }
+
+    function handleGameStarted({ player1, player2, numberOfRows, numberOfColumns }) {
+        setGameStatus(GameStatus.IN_PROGRESS);
+        setNumberOfRows(numberOfRows);
+        setNumberOfColumns(numberOfColumns);
+
+        if (socket.id === player1) {
+            setPlayer('X');
+            setInGameStatus(InGameStatus.YOUR_TURN);
+        } else if (socket.id === player2) {
+            setPlayer('O');
+            setInGameStatus(InGameStatus.OPPONENT_TURN);
+        } else {
+            console.error('Something went wrong');
+            return;
+        }
+
+        setBoard(Array.from({ length: numberOfRows }, () => Array(numberOfColumns).fill(null)));
+    }
+
+    function handleBoardUpdated({ board: newBoard, winner }) {
+        setBoard(newBoard);
+
+        if (inGameStatusRef.current === InGameStatus.YOUR_TURN) {
+            setInGameStatus(InGameStatus.OPPONENT_TURN);
+        } else if (inGameStatusRef.current === InGameStatus.OPPONENT_TURN) {
+            setInGameStatus(InGameStatus.YOUR_TURN);
+        }
+
+        if (winner) {
+            if (winner === playerRef.current) {
+                setInGameStatus(InGameStatus.YOU_WON);
+            } else if (winner === 'DRAW') {
+                setInGameStatus(InGameStatus.DRAW);
+            } else {
+                setInGameStatus(InGameStatus.YOU_LOST);
+            }
+            setGameStatus(GameStatus.READY_TO_START);
+        }
+    }
+
+    function handleOpponentDisconnected() {
+        setGameStatus(GameStatus.READY_TO_START);
+        setInGameStatus(InGameStatus.OPPONENT_DISCONNECTED);
+    }
+
+    useEffect(() => {
+        inGameStatusRef.current = inGameStatus;
+        playerRef.current = player;
+    }, [inGameStatus, player]);
+
+    useEffect(() => {
+        socket.on('game_started', handleGameStarted);
+        socket.on('board_updated', handleBoardUpdated);
+        socket.on('opponent_disconnected', handleOpponentDisconnected);
+
+        return () => {
+            socket.off('game_started');
+            socket.off('board_updated');
+            socket.off('opponent_disconnected');
+        };
+    }, [socket]);
+
+    return { board, handleClick, numberOfRows, numberOfColumns, startGame, gameStatus, inGameStatus };
+}
+
+export default useTicTacToe;
